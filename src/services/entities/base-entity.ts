1df64eb92292c335e5dc87ada404581e26578741
wangyong.li <wangyong.li@yunlsp.com>
2020-03-31 17:32:23 +0800

    提交泛型

--- src/services/entities/base-entity.ts
+++ src/services/entities/base-entity.ts
@@ -1,22 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { getConnection, InsertResult } from 'typeorm';
+import { getConnection, InsertResult, Repository } from 'typeorm';
 @Injectable()
 export class BaseEntity<T> {
-  entity;
-  constructor(entity) {
-    this.entity = entity;
-  }
+  constructor(private readonly entity) {}
   find(argsSearch = {}): Promise<T> {
     return this.entity.findOne(argsSearch);
   }
   findAll(args = {}): Promise<T[]> {
-    return this.entity.findAll(args);
+    return this.entity.find(args);
   }
   /**
    * 批量insert数据
    * @bulkData 批量数据
    */
-  async batchEventInsert(bulkData: T[]): Promise<InsertResult> {
+  async batchEventInsert(bulkData: []): Promise<InsertResult> {
     return await getConnection()
       .createQueryBuilder()
       .insert()
@@ -28,7 +25,7 @@ export class BaseEntity<T> {
    *  单个insert数据
    * @bulkData 单个数据
    */
-  async SingOneInsert(singData: T | any): Promise<any> {
+  async SingOneInsert(singData): Promise<any> {
     return await this.entity.save(singData);
   }
 }
